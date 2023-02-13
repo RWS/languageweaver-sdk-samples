6943d46fe3097deaa5809c1fd539960f641cc92b
alungu <alungu@sdl.com>
2023-02-13 13:33:32 +0200

    EE-21764 - SDK - C# - Add code samples for Feedback CRUD

---
+++ dotnet/Samples/Feedback/EdgeFeedbackService.cs
@@ -0,0 +1,151 @@
+﻿using LanguageWeaver.Sdk.Common.Edge;
+using LanguageWeaver.Sdk.Constants;
+using LanguageWeaver.Sdk.Feedback.Edge.Request;
+using LanguageWeaver.Sdk.Feedback.Edge.Result;
+
+namespace LanguageWeaver.Sdk.Samples.Feedback;
+
+public class EdgeFeedbackService
+{
+    public static void Main()
+    {
+        using var lwClient = new EdgeLanguageWeaverClient().Build();
+
+        EdgeFeedbackResult feedback;
+
+        //CREATE FEEDBACK
+        feedback = CreateFeedback();
+        Console.WriteLine(feedback.FeedbackId);
+
+        //UPDATE FEEDBACK
+        EdgeUpdateFeedbackRequest edgeUpdateFeedbackRequest = feedback.ToUpdateRequest();
+        edgeUpdateFeedbackRequest.SuggestedTranslation = "new suggested translation";
+        edgeUpdateFeedbackRequest.ClearComments().AddComment(FeedbackComment.CapitalizationPunctuation);
+        feedback = lwClient.UpdateFeedback(edgeUpdateFeedbackRequest);
+
+        //UPDATE FEEDBACK APPROVAL
+        EdgeUpdateFeedbackApprovalRequest edgeUpdateFeedbackApprovalRequest =
+            feedback.ToUpdateApprovalRequest(ApprovalStatus.Rejected);
+        feedback = lwClient.UpdateFeedbackApproval(edgeUpdateFeedbackApprovalRequest);
+        Console.WriteLine(feedback.ApprovalState);
+
+        //DELETE FEEDBACK
+        EdgeDeleteFeedbackRequest deleteFeedbackRequest = feedback.ToDeleteRequest();
+        lwClient.DeleteFeedback(deleteFeedbackRequest);
+    }
+
+    public static EdgeFeedbackResult CreateFeedback()
+    {
+        using var lwClient = new EdgeLanguageWeaverClient().Build();
+
+        EdgeCreateFeedbackRequest edgeCreateFeedbackRequest = new EdgeCreateFeedbackRequest()
+        {
+            LanguagePairId = "languagePairId",
+            SourceLanguageId = "eng",
+            TargetLanguageId = "fra",
+            Model = "generic",
+            SourceText = "sourceText",
+            TargetText = "targetText",
+            SuggestedTranslation = "suggestedTranslation2",
+            ApprovalState = ApprovalStatus.Approved
+        };
+        edgeCreateFeedbackRequest.AddComment(FeedbackComment.Spelling);
+
+        EdgeFeedbackResult feedback = lwClient.CreateFeedback(edgeCreateFeedbackRequest);
+        return feedback;
+    }
+
+    public static void UpdateFeedback()
+    {
+        using var lwClient = new EdgeLanguageWeaverClient().Build();
+
+        EdgeUpdateFeedbackRequest edgeUpdateFeedbackRequest = new EdgeUpdateFeedbackRequest()
+        {
+            FeedbackId = "feedbackId",
+            SuggestedTranslation = "new suggested translation"
+        };
+        edgeUpdateFeedbackRequest.AddComment(FeedbackComment.CapitalizationPunctuation);
+
+        EdgeFeedbackResult feedback = lwClient.UpdateFeedback(edgeUpdateFeedbackRequest);
+
+        Console.WriteLine(feedback.ApprovalState);
+    }
+
+    public static void UpdateFeedbackStatus()
+    {
+        using var lwClient = new EdgeLanguageWeaverClient().Build();
+
+        EdgeUpdateFeedbackApprovalRequest edgeUpdateFeedbackApprovalRequest = new EdgeUpdateFeedbackApprovalRequest()
+        {
+            FeedbackId = "feedbackId",
+            ApprovalStatus = ApprovalStatus.Approved
+        };
+
+        EdgeFeedbackResult feedback = lwClient.UpdateFeedbackApproval(edgeUpdateFeedbackApprovalRequest);
+
+        Console.WriteLine(feedback.ApprovalState);
+    }
+
+    public static void DeleteFeedback()
+    {
+        using var lwClient = new EdgeLanguageWeaverClient().Build();
+
+        EdgeDeleteFeedbackRequest deleteFeedbackRequest = new EdgeDeleteFeedbackRequest()
+        {
+            FeedbackId = "feedbackId"
+        };
+
+        lwClient.DeleteFeedback(deleteFeedbackRequest);
+    }
+
+    public static void RetrieveFeedback()
+    {
+        using var lwClient = new EdgeLanguageWeaverClient().Build();
+
+        int pageNumber = 1;
+        int pageSize = 50;
+        EdgeFeedbackListResult edgeFeedbackList;
+        do
+        {
+            edgeFeedbackList = lwClient.GetEdgeFeedback(pageNumber, pageSize);
+            //process feedback
+            List<EdgeFeedbackResult> feedbackList = edgeFeedbackList.FeedbackList;
+            feedbackList.Take(3)
+                .ToList()
+                .ForEach(edgeFeedbackResult => Console.WriteLine(edgeFeedbackResult.FeedbackId));
+            pageNumber++;
+        } while (edgeFeedbackList.FeedbackList.Count == pageSize);
+    }
+
+    public static void FilterFeedback()
+    {
+        using var lwClient = new EdgeLanguageWeaverClient().Build();
+
+        EdgeFilterFeedbackRequest filterFeedbackRequest = new EdgeFilterFeedbackRequest()
+        {
+            LanguagePairId = "languagePairId",
+            SourceText = "sourceText",
+            MachineTranslation = "machineTranslation",
+            SuggestedTranslation = "suggestedTranslation",
+            Comment = "comment",
+            Reviewer = "reviewer username",
+            User = "creation username",
+            ApprovalStatus = ApprovalStatus.Approved,
+            StartDate = "10/10/2021",
+            EndDate = "10/10/2022",
+        };
+
+        int pageNumber = 1;
+        EdgeFeedbackListResult filteredFeedback;
+        do
+        {
+            filteredFeedback = lwClient.GetEdgeFeedback(filterFeedbackRequest, pageNumber);
+            //process feedback
+            List<EdgeFeedbackResult> feedbackList = filteredFeedback.FeedbackList;
+            feedbackList.Take(3)
+                .ToList()
+                .ForEach(edgeFeedbackResult => Console.WriteLine(edgeFeedbackResult.FeedbackId));
+            pageNumber++;
+        } while (filteredFeedback.FeedbackList.Count > 0);
+    }
+}
---
+++ dotnet/Samples/Feedback/FeedbackService.cs
@@ -0,0 +1,143 @@
+﻿using LanguageWeaver.Sdk.Common;
+using LanguageWeaver.Sdk.Configurations;
+using LanguageWeaver.Sdk.Constants;
+using LanguageWeaver.Sdk.Feedback.Common.Request;
+using LanguageWeaver.Sdk.Feedback.Common.Result;
+
+namespace LanguageWeaver.Sdk.Samples.Feedback;
+
+public class FeedbackService
+{
+    public static void Main()
+    {
+        using var lwClient = new SdkFactory().GetLanguageWeaverClient(new ClientConfiguration());
+
+        FeedbackResult feedback;
+
+        //CREATE FEEDBACK
+        feedback = CreateFeedback();
+        Console.WriteLine(feedback.FeedbackId);
+
+        //UPDATE FEEDBACK
+        UpdateFeedbackRequest updateFeedbackRequest = feedback.ToUpdateRequest();
+        updateFeedbackRequest.SuggestedTranslation = "new suggested translation";
+        updateFeedbackRequest.ClearComments().AddComment(FeedbackComment.CapitalizationPunctuation);
+        feedback = lwClient.UpdateFeedback(updateFeedbackRequest);
+
+        //UPDATE FEEDBACK APPROVAL
+        UpdateFeedbackApprovalRequest updateFeedbackApprovalRequest =
+            feedback.ToUpdateApprovalRequest(ApprovalStatus.Rejected);
+        feedback = lwClient.UpdateFeedbackApproval(updateFeedbackApprovalRequest);
+        Console.WriteLine(feedback.ApprovalState);
+
+        //DELETE FEEDBACK
+        DeleteFeedbackRequest deleteFeedbackRequest = feedback.ToDeleteRequest();
+        lwClient.DeleteFeedback(deleteFeedbackRequest);
+    }
+
+    public static FeedbackResult CreateFeedback()
+    {
+        using var lwClient = new SdkFactory().GetLanguageWeaverClient(new ClientConfiguration());
+
+        CreateFeedbackRequest createFeedbackRequest = new CreateFeedbackRequest()
+        {
+            SourceLanguageId = "eng",
+            TargetLanguageId = "fra",
+            Model = "generic",
+            SourceText = "sourceText",
+            TargetText = "targetText",
+            SuggestedTranslation = "suggestedTranslation",
+        };
+        createFeedbackRequest.AddComment(FeedbackComment.Spelling);
+
+        FeedbackResult feedback = lwClient.CreateFeedback(createFeedbackRequest);
+
+        return feedback;
+    }
+
+    public static void UpdateFeedback()
+    {
+        using var lwClient = new SdkFactory().GetLanguageWeaverClient(new ClientConfiguration());
+
+        UpdateFeedbackRequest updateFeedbackRequest = new UpdateFeedbackRequest()
+        {
+            FeedbackId = "feedbackId",
+            SuggestedTranslation = "new suggested translation"
+        };
+
+        updateFeedbackRequest.AddComment(FeedbackComment.CapitalizationPunctuation);
+
+        FeedbackResult feedback = lwClient.UpdateFeedback(updateFeedbackRequest);
+    }
+
+    public static void UpdateFeedbackStatus()
+    {
+        using var lwClient = new SdkFactory().GetLanguageWeaverClient(new ClientConfiguration());
+
+        UpdateFeedbackApprovalRequest updateFeedbackApprovalRequest = new UpdateFeedbackApprovalRequest()
+        {
+            FeedbackId = "feedbackId",
+            ApprovalStatus = ApprovalStatus.Approved
+        };
+
+        FeedbackResult feedback = lwClient.UpdateFeedbackApproval(updateFeedbackApprovalRequest);
+        Console.WriteLine(feedback.ApprovalState);
+    }
+
+    public static void DeleteFeedback()
+    {
+        using var lwClient = new SdkFactory().GetLanguageWeaverClient(new ClientConfiguration());
+
+        DeleteFeedbackRequest deleteFeedbackRequest = new DeleteFeedbackRequest()
+        {
+            FeedbackId = "feedbackId"
+        };
+
+        lwClient.DeleteFeedback(deleteFeedbackRequest);
+    }
+
+    public static void RetrieveFeedback()
+    {
+        using var lwClient = new SdkFactory().GetLanguageWeaverClient(new ClientConfiguration());
+
+        int pageNumber = 1;
+        int pageSize = 50;
+        FeedbackListResult feedbackListResult;
+        do
+        {
+            feedbackListResult = lwClient.GetFeedback(pageNumber, pageSize);
+            //process feedback
+            List<FeedbackResult> feedbackList = feedbackListResult.FeedbackList;
+            feedbackList.Take(3)
+                .ToList()
+                .ForEach(feedbackResult => Console.WriteLine(feedbackResult.FeedbackId));
+            pageNumber++;
+        } while (feedbackListResult.FeedbackList.Count == pageSize);
+    }
+
+    public static void FilterFeedback()
+    {
+        using var lwClient = new SdkFactory().GetLanguageWeaverClient(new ClientConfiguration());
+
+        FilterFeedbackRequest filterFeedbackRequest = new FilterFeedbackRequest()
+        {
+            ApprovalStatus = ApprovalStatus.Approved,
+            User = "userId",
+            StartDate = "10/10/2021",
+            EndDate = "10/10/2022"
+        };
+
+        int pageNumber = 1;
+        FeedbackListResult filteredFeedback;
+        do
+        {
+            filteredFeedback = lwClient.GetFeedback(filterFeedbackRequest, pageNumber);
+            //process feedback
+            List<FeedbackResult> feedbackList = filteredFeedback.FeedbackList;
+            feedbackList.Take(3)
+                .ToList()
+                .ForEach(feedbackResult => Console.WriteLine(feedbackResult.FeedbackId));
+            pageNumber++;
+        } while (filteredFeedback.FeedbackList.Count > 0);
+    }
+}
