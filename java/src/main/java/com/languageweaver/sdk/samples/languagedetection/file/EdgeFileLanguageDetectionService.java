e0064ec4dffd37bea751a09b3abbc6ae7e75f9b6
alungu <alungu@sdl.com>
2023-04-06 14:52:11 +0300

    EE-24158 - SDK - Java - Language Detection - Samples

---
+++ java/src/main/java/com/languageweaver/sdk/samples/languagedetection/file/EdgeFileLanguageDetectionService.java
@@ -0,0 +1,26 @@
+package com.languageweaver.sdk.samples.languagedetection.file;
+
+import com.languageweaver.sdk.common.constants.Format;
+import com.languageweaver.sdk.common.edge.EdgeLanguageWeaverClient;
+import com.languageweaver.sdk.languagedetection.edge.request.EdgeFileLanguageDetectionRequest;
+import com.languageweaver.sdk.languagedetection.edge.result.EdgeLanguageDetectionResult;
+
+import java.nio.file.Paths;
+
+public class EdgeFileLanguageDetectionService {
+    public static void main(String[] args) throws Exception {
+        try (EdgeLanguageWeaverClient lwClient = new EdgeLanguageWeaverClient().build()) {
+            EdgeFileLanguageDetectionRequest edgeFileLanguageDetectionRequest = new EdgeFileLanguageDetectionRequest()
+                    .setInputFile(Paths.get("java", "src", "main", "resources", "input", "input1.txt").toFile().getAbsolutePath())
+                    .setInputFormat(Format.PLAIN)
+                    .setEncoding("UTF-8");
+
+            final EdgeLanguageDetectionResult edgeLanguageDetectionResult = lwClient.languageDetectionFile(edgeFileLanguageDetectionRequest);
+
+            if (edgeLanguageDetectionResult.getLanguages() != null) {
+                edgeLanguageDetectionResult.getLanguages()
+                        .forEach(language -> System.out.println(language.getName()));
+            }
+        }
+    }
+}
---
+++ java/src/main/java/com/languageweaver/sdk/samples/languagedetection/text/EdgeTextLanguageDetectionService.java
@@ -0,0 +1,24 @@
+package com.languageweaver.sdk.samples.languagedetection.text;
+
+import com.languageweaver.sdk.common.constants.Format;
+import com.languageweaver.sdk.common.edge.EdgeLanguageWeaverClient;
+import com.languageweaver.sdk.languagedetection.edge.request.EdgeTextLanguageDetectionRequest;
+import com.languageweaver.sdk.languagedetection.edge.result.EdgeLanguageDetectionResult;
+
+public class EdgeTextLanguageDetectionService {
+    public static void main(String[] args) throws Exception {
+        try (EdgeLanguageWeaverClient lwClient = new EdgeLanguageWeaverClient().build()) {
+            EdgeTextLanguageDetectionRequest edgeTextLanguageDetectionRequest = new EdgeTextLanguageDetectionRequest()
+                    .setInput("A mountain is generally steeper than a hill")
+                    .setInputFormat(Format.PLAIN)
+                    .setEncoding("UTF-8");
+
+            final EdgeLanguageDetectionResult edgeLanguageDetectionResult = lwClient.languageDetectionText(edgeTextLanguageDetectionRequest);
+
+            if (edgeLanguageDetectionResult.getLanguages() != null) {
+                edgeLanguageDetectionResult.getLanguages()
+                        .forEach(language -> System.out.println(language.getName()));
+            }
+        }
+    }
+}
