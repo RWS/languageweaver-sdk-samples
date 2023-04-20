8ab4c650a797d5d5aad2036b2027e10a79cbac31
alungu99 <111429259+alungu99@users.noreply.github.com>
2023-04-20 10:49:39 +0300

    EE-24159 - SDK - C# - Language Detection - Samples

---
+++ dotnet/Samples/LanguageDetection/Text/CloudTextLanguageDetectionService.cs
@@ -0,0 +1,24 @@
+﻿using LanguageWeaver.Sdk.Common.Cloud;
+using LanguageWeaver.Sdk.Constants;
+using LanguageWeaver.Sdk.LanguageDetection.Cloud.Request;
+
+namespace LanguageWeaver.Sdk.Samples.LanguageDetection.Text;
+
+public class CloudLanguageDetectionService
+{
+    public static void Main()
+    {
+        using var lwClient = new CloudLanguageWeaverClient().Build();
+
+        var cloudTextLanguageDetectionRequest = new CloudTextLanguageDetectionRequest()
+        {
+            Input = "A mountain is generally steeper than a hill",
+            InputFormat = Format.Plain
+        };
+
+        var cloudLanguageDetectionResult = lwClient.DetectLanguage(cloudTextLanguageDetectionRequest);
+
+        cloudLanguageDetectionResult.Languages
+            .ForEach(language => Console.WriteLine(language.Name));
+    }
+}
---
+++ dotnet/Samples/LanguageDetection/Text/EdgeTextLanguageDetectionService.cs
@@ -0,0 +1,24 @@
+﻿using LanguageWeaver.Sdk.Common.Edge;
+using LanguageWeaver.Sdk.Constants;
+using LanguageWeaver.Sdk.LanguageDetection.Edge.Request;
+
+namespace LanguageWeaver.Sdk.Samples.LanguageDetection.Text;
+
+public class EdgeLanguageDetectionService
+{
+    public static void Main()
+    {
+        using var lwClient = new EdgeLanguageWeaverClient().Build();
+        var edgeTextLanguageDetectionRequest = new EdgeTextLanguageDetectionRequest()
+        {
+            Input = "A mountain is generally steeper than a hill",
+            InputFormat = Format.Plain,
+            Encoding = "UTF-8"
+        };
+
+        var edgeLanguageDetectionResult = lwClient.DetectLanguage(edgeTextLanguageDetectionRequest);
+
+        edgeLanguageDetectionResult.Languages
+            .ForEach(language => Console.WriteLine(language.Name));
+    }
+}
